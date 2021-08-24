eae34fa541d30aac2dcae71291b0f55e3e6f1886
nalbam <me@nalbam.com>
2021-08-24 17:43:36 +0900

    add eks-demo-b

---
+++ eks-demo-b/04-backend.tf
@@ -0,0 +1,27 @@
+# backend
+
+terraform {
+  required_version = "1.0.4"
+
+  backend "s3" {
+    region         = "ap-northeast-2"
+    bucket         = "terraform-workshop-082867736673"
+    key            = "backend/terraform-env-bruce/demo/eks-demo-b/terraform.tfstate"
+    dynamodb_table = "terraform-resource-lock"
+    encrypt        = true
+  }
+
+  #   backend "remote" {
+  #     organization = "bruce"
+  #     workspaces {
+  #       name = "eks-demo"
+  #     }
+  #   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.53.0"
+    }
+  }
+}
---
+++ eks-demo-b/30-eks.tf
@@ -0,0 +1,22 @@
+# eks
+
+module "eks" {
+  source  = "nalbam/eks/aws"
+  version = "0.14.2"
+
+  cluster_name = var.cluster_name
+
+  kubernetes_version = var.cluster_version
+
+  vpc_id     = local.vpc_id
+  subnet_ids = local.subnet_ids
+
+  endpoint_public_access = true
+
+  iam_group = local.iam_group
+  iam_roles = local.iam_roles
+
+  # save_local_files = true
+
+  tags = local.tags
+}
