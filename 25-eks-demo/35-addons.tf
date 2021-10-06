50906a669116ce3dc3e507dfe4b231610322b1cb
nalbam <me@nalbam.com>
2021-10-06 09:09:57 +0900

    add eks addons

---
+++ 25-eks-demo/35-addons.tf
@@ -0,0 +1,19 @@
+# addons
+
+resource "aws_eks_addon" "kube-proxy" {
+  cluster_name  = module.eks.cluster_name
+  addon_name    = "kube-proxy"
+  addon_version = "v1.21.2-eksbuild.2"
+}
+
+resource "aws_eks_addon" "vpc-cni" {
+  cluster_name  = module.eks.cluster_name
+  addon_name    = "vpc-cni"
+  addon_version = "v1.9.1-eksbuild.1"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name  = module.eks.cluster_name
+  addon_name    = "coredns"
+  addon_version = "v1.8.4-eksbuild.1"
+}
