18b1fbe21a8205bd957a3299de9954e2c1b896c1
nalbam <me@nalbam.com>
2021-11-07 20:01:51 +0900

    aws provider 3.64.2

--- 25-eks-demo/04-backend.tf
+++ 25-eks-demo/04-backend.tf
@@ -14,7 +14,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.64.1"
+      version = "3.64.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
