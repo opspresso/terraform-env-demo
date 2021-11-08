82d873b4577e65d9dab902132d77bcd2e7c9680a
nalbam <me@nalbam.com>
2021-11-08 09:38:51 +0900

    aws_eks_addon versions

--- 25-eks-demo/70-addons.tf
+++ 25-eks-demo/70-addons.tf
@@ -1,5 +1,8 @@
 # addons
-# aws eks describe-addon-versions
+# aws eks describe-addon-versions | jq .
+# aws eks describe-addon-versions | jq '.addons[] | select(.addonName=="coredns") | .addonVersions[].addonVersion' -r | sort -V -r | head -1
+# aws eks describe-addon-versions | jq '.addons[] | select(.addonName=="kube-proxy") | .addonVersions[].addonVersion' -r | sort -V -r | head -1
+# aws eks describe-addon-versions | jq '.addons[] | select(.addonName=="vpc-cni") | .addonVersions[].addonVersion' -r | sort -V -r | head -1
 
 resource "aws_eks_addon" "kube-proxy" {
   cluster_name  = module.eks.cluster_name
@@ -11,20 +14,20 @@ resource "aws_eks_addon" "kube-proxy" {
   ]
 }
 
-resource "aws_eks_addon" "vpc-cni" {
+resource "aws_eks_addon" "coredns" {
   cluster_name  = module.eks.cluster_name
-  addon_name    = "vpc-cni"
-  addon_version = "v1.9.3-eksbuild.1"
+  addon_name    = "coredns"
+  addon_version = "v1.8.4-eksbuild.1"
 
   depends_on = [
     module.workers-v3.worker_asg_id
   ]
 }
 
-resource "aws_eks_addon" "coredns" {
+resource "aws_eks_addon" "vpc-cni" {
   cluster_name  = module.eks.cluster_name
-  addon_name    = "coredns"
-  addon_version = "v1.8.4-eksbuild.1"
+  addon_name    = "vpc-cni"
+  addon_version = "v1.9.3-eksbuild.1"
 
   depends_on = [
     module.workers-v3.worker_asg_id
