f27ff3e7ff824f35c91c6fd7e8349bb4cbf28edc
nalbam <me@nalbam.com>
2021-11-08 16:07:42 +0900

    add eks-demo-b

---
+++ 25-eks-demo-b/00-variable.tf
@@ -0,0 +1,20 @@
+# variable
+
+variable "region" {
+  description = "리전을 입력 합니다. e.g: ap-northeast-2"
+  default     = "ap-northeast-2"
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster 이름을 입력합니다."
+  default     = "eks-demo-b"
+}
+
+variable "cluster_version" {
+  description = "쿠버네티스 버전을 입력합니다."
+  default     = "1.21"
+}
+
+variable "key_name" {
+  default = "bruce-seoul"
+}
