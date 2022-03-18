82df3c445acbb283f21cb60924530d607a3def33
nalbam <me@nalbam.com>
2022-03-18 16:37:27 +0900

    private

--- 25-eks-demo/60-asg-private-v1.tf
+++ 25-eks-demo/60-asg-private-v1.tf
@@ -1,34 +1,34 @@
-# worker workers
+# # worker workers
 
-module "private-v1" {
-  source  = "nalbam/eks-worker/aws"
-  version = "0.14.14"
+# module "private-v1" {
+#   source  = "nalbam/eks-worker/aws"
+#   version = "0.14.14"
 
-  name    = "private"
-  subname = "v1"
+#   name    = "private"
+#   subname = "v1"
 
-  cluster_info = module.eks.cluster_info
+#   cluster_info = module.eks.cluster_info
 
-  role_name       = local.worker_role_name
-  security_groups = local.worker_security_groups
-  subnet_ids      = local.intra_subnets
+#   role_name       = local.worker_role_name
+#   security_groups = local.worker_security_groups
+#   subnet_ids      = local.intra_subnets
 
-  worker_ami_arch    = "x86_64"
-  worker_ami_keyword = "*"
+#   worker_ami_arch    = "x86_64"
+#   worker_ami_keyword = "*"
 
-  key_name = var.key_name
+#   key_name = var.key_name
 
-  enable_autoscale = true
-  enable_taints    = true
-  enable_spot      = true
-  enable_event     = true
+#   enable_autoscale = true
+#   enable_taints    = true
+#   enable_spot      = true
+#   enable_event     = true
 
-  instance_type = "c6i.large"
-  volume_type   = "gp3"
-  volume_size   = "30"
+#   instance_type = "c6i.large"
+#   volume_type   = "gp3"
+#   volume_size   = "30"
 
-  min = 1
-  max = 2
+#   min = 1
+#   max = 2
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
