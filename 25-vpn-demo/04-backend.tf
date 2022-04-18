35c7c37ecf126a8f896d360f534042e7c95161dc
nalbam <me@nalbam.com>
2022-04-18 13:56:15 +0900

    tf 1.1.8

--- 25-vpn-demo/04-backend.tf
+++ 25-vpn-demo/04-backend.tf
@@ -1,7 +1,7 @@
 # backend
 
 terraform {
-  required_version = "1.1.5"
+  required_version = "1.1.8"
 
   backend "s3" {
     region         = "ap-northeast-2"
@@ -14,7 +14,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.74.1"
+      version = "4.9.0"
     }
   }
 }
