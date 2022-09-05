652730f64c2d8df1f7ef16a5c63e6f58e66ba67d
nalbam <me@nalbam.com>
2022-09-05 21:22:38 +0900

    enable_ephemeral_storage

--- 22-atlantis/20-main.tf
+++ 22-atlantis/20-main.tf
@@ -18,7 +18,18 @@ module "atlantis" {
   route53_zone_name = var.domain
   certificate_arn   = data.aws_acm_certificate.domain.arn
 
+  # EFS
+  enable_ephemeral_storage = true
+
   # ECS
+  ecs_service_platform_version = "LATEST"
+  ecs_container_insights       = true
+  ecs_task_cpu                 = 512
+  ecs_task_memory              = 1024
+  container_memory_reservation = 256
+  container_cpu                = 512
+  container_memory             = 1024
+
   runtime_platform = {
     operating_system_family = "LINUX"
     cpu_architecture        = "ARM64"
@@ -45,11 +56,12 @@ module "atlantis" {
   # Trusted roles
   trusted_principals = ["ssm.amazonaws.com"]
 
-  # Atlantis github
+  # Github token
   atlantis_github_user       = var.atlantis_github_user
   atlantis_github_user_token = data.aws_ssm_parameter.github_token.value
   atlantis_repo_allowlist    = var.atlantis_repo_allowlist
 
+  # Google auth
   alb_authenticate_oidc = {
     issuer                              = "https://accounts.google.com"
     token_endpoint                      = "https://oauth2.googleapis.com/token"
