ae89831edc28ad8c0d148029eaa3d21f1804da74
nalbam <me@nalbam.com>
2022-09-05 23:47:56 +0900

    atlantis_image

--- 22-atlantis/20-main.tf
+++ 22-atlantis/20-main.tf
@@ -56,9 +56,9 @@ module "atlantis" {
   # Trusted roles
   trusted_principals = ["ssm.amazonaws.com"]
 
-  # Atlantis
-  atlantis_image   = "infracost/infracost-atlantis"
-  atlantis_version = "latest"
+  # # Infracost
+  # atlantis_image   = "infracost/infracost-atlantis"
+  # atlantis_version = "latest"
 
   # Github token
   atlantis_github_user       = var.atlantis_github_user
