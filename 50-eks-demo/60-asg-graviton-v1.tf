58921d91da206deb683ee49a58c5560d28eba9f9
nalbam <me@nalbam.com>
2022-09-06 13:22:32 +0900

    graviton

--- 50-eks-demo/60-asg-graviton-v1.tf
+++ 50-eks-demo/60-asg-graviton-v1.tf
@@ -1,34 +1,34 @@
-# # worker graviton
+# worker graviton
 
-# module "graviton-v1" {
-#   source = "nalbam/eks-worker/aws"
-#   # version = "1.0.1"
+module "graviton-v1" {
+  source = "nalbam/eks-worker/aws"
+  # version = "1.0.1"
 
-#   name    = "graviton"
-#   subname = "v1"
+  name    = "graviton"
+  subname = "v1"
 
-#   cluster_info = module.eks.cluster_info
+  cluster_info = module.eks.cluster_info
 
-#   role_name       = local.worker_role_name
-#   security_groups = local.worker_security_groups
-#   subnet_ids      = local.private_subnets
+  role_name       = local.worker_role_name
+  security_groups = local.worker_security_groups
+  subnet_ids      = local.private_subnets
 
-#   worker_ami_arch    = "arm64"
-#   worker_ami_keyword = "*"
+  worker_ami_arch    = "arm64"
+  worker_ami_keyword = "*"
 
-#   key_name = var.key_name
+  key_name = var.key_name
 
-#   enable_autoscale = true
-#   enable_taints    = true
-#   enable_spot      = true
-#   enable_event     = true
+  enable_autoscale = true
+  enable_taints    = true
+  enable_spot      = true
+  enable_event     = true
 
-#   instance_type = "c6g.large"
-#   volume_type   = "gp3"
-#   volume_size   = "50"
+  instance_type = "c6g.large"
+  volume_type   = "gp3"
+  volume_size   = "50"
 
-#   min = 1
-#   max = 2
+  min = 1
+  max = 2
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
--- 50-eks-demo/60-asg-intra-v1.tf
+++ 50-eks-demo/60-asg-intra-v1.tf
@@ -1,34 +1,34 @@
-# worker intra
+# # worker intra
 
-module "intra-v1" {
-  source = "nalbam/eks-worker/aws"
-  # version = "1.0.1"
+# module "intra-v1" {
+#   source = "nalbam/eks-worker/aws"
+#   # version = "1.0.1"
 
-  name    = "intra"
-  subname = "v1"
+#   name    = "intra"
+#   subname = "v1"
 
-  cluster_info = module.eks.cluster_info
+#   cluster_info = module.eks.cluster_info
 
-  role_name       = local.worker_role_name
-  security_groups = local.worker_security_groups
-  subnet_ids      = local.intra_subnets
+#   role_name       = local.worker_role_name
+#   security_groups = local.worker_security_groups
+#   subnet_ids      = local.intra_subnets
 
-  worker_ami_arch    = "x86_64"
-  worker_ami_keyword = "*"
+#   worker_ami_arch    = "x86_64"
+#   worker_ami_keyword = "*"
 
-  key_name = var.key_name
+#   key_name = var.key_name
 
-  enable_autoscale = true
-  enable_taints    = true
-  enable_spot      = true
-  enable_event     = true
+#   enable_autoscale = true
+#   enable_taints    = true
+#   enable_spot      = true
+#   enable_event     = true
 
-  instance_type = "c6i.large"
-  volume_type   = "gp3"
-  volume_size   = "50"
+#   instance_type = "c6i.large"
+#   volume_type   = "gp3"
+#   volume_size   = "50"
 
-  min = 1
-  max = 2
+#   min = 1
+#   max = 2
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
