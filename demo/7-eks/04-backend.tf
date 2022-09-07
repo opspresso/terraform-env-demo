7c40ae6f760d0a67d0c9b6d7cdbceb5384712a31
nalbam <me@nalbam.com>
2022-09-07 11:24:40 +0900

    terraform kubernetes provider version

--- demo/7-eks/04-backend.tf
+++ demo/7-eks/04-backend.tf
@@ -18,7 +18,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.11.0"
+      version = "2.11.0" # terraform kubernetes provider version
     }
   }
 }
