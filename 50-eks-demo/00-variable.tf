7fe5e53867aff47cef118e3d424bd6b933f4b07b
nalbam <me@nalbam.com>
2022-09-07 10:12:07 +0900

    eks 1.22

--- 50-eks-demo/00-variable.tf
+++ 50-eks-demo/00-variable.tf
@@ -12,15 +12,20 @@ variable "cluster_name" {
 
 variable "kubernetes_version" {
   description = "쿠버네티스 버전을 입력합니다."
-  default     = "1.23"
+  default     = "1.22"
 }
 
 variable "addons_version" {
   default = {
-    "aws-ebs-csi-driver" : "v1.10.0-eksbuild.1"
-    "coredns" : "v1.8.7-eksbuild.2"
-    "kube-proxy" : "v1.23.7-eksbuild.1"
-    "vpc-cni" : "v1.11.3-eksbuild.1"
+    # for 1.22
+    "kube-proxy" : "v1.22.6-eksbuild.1"
+    "coredns" : "v1.8.7-eksbuild.1"
+    "vpc-cni" : "v1.10.3-eksbuild.1"
+    # # for 1.23
+    # "aws-ebs-csi-driver" : "v1.10.0-eksbuild.1"
+    # "coredns" : "v1.8.7-eksbuild.2"
+    # "kube-proxy" : "v1.23.7-eksbuild.1"
+    # "vpc-cni" : "v1.11.3-eksbuild.1"
   }
 }
 
