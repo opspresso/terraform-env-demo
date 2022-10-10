227aed7f2474fb85101ecad621a1aa945ed75c73
nalbam <me@nalbam.com>
2022-10-10 09:58:29 +0900

    enable_spot

--- demo/7-eks/60-asg-workers-v1.tf
+++ demo/7-eks/60-asg-workers-v1.tf
@@ -20,7 +20,7 @@ module "workers-v1" {
 
   enable_autoscale = true
   enable_event     = true
-  enable_spot      = false
+  enable_spot      = true
   enable_taints    = false
 
   instance_type = "c6i.large"
