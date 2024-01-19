6b66b267af1c57e3c58dfef7750395f7594b67cf
nalbam <me@nalbam.com>
2024-01-20 00:59:32 +0900

    rm graviton

--- demo/7-eks/60-asg-graviton-v1.tf
+++ demo/7-eks/60-asg-graviton-v1.tf
@@ -1,46 +1,46 @@
-# worker graviton
+# # worker graviton
 
-module "graviton-v1" {
-  source = "nalbam/eks-worker/aws"
-  # version = "~> 3.0"
+# module "graviton-v1" {
+#   source = "nalbam/eks-worker/aws"
+#   # version = "~> 3.0"
 
-  name    = "graviton"
-  subname = "v1"
+#   name    = "graviton"
+#   subname = "v1"
 
-  region     = local.region
-  account_id = local.account_id
+#   region     = local.region
+#   account_id = local.account_id
 
-  cluster_name = local.cluster_name
+#   cluster_name = local.cluster_name
 
-  cluster_endpoint              = module.eks.cluster_endpoint
-  cluster_certificate_authority = module.eks.cluster_certificate_authority
+#   cluster_endpoint              = module.eks.cluster_endpoint
+#   cluster_certificate_authority = module.eks.cluster_certificate_authority
 
-  ami_arch        = "arm64"
-  role_name       = local.worker_role_name
-  security_groups = local.worker_security_groups
-  subnet_ids      = local.private_subnets
+#   ami_arch        = "arm64"
+#   role_name       = local.worker_role_name
+#   security_groups = local.worker_security_groups
+#   subnet_ids      = local.private_subnets
 
-  kubernetes_version = var.kubernetes_version
+#   kubernetes_version = var.kubernetes_version
 
-  key_name = var.key_name
+#   key_name = var.key_name
 
-  enable_autoscale = true
-  enable_mixed     = true
-  enable_taints    = true
+#   enable_autoscale = true
+#   enable_mixed     = true
+#   enable_taints    = true
 
-  on_demand_base = 0
-  on_demand_rate = 0
+#   on_demand_base = 0
+#   on_demand_rate = 0
 
-  mixed_instances = ["c6g.large"]
-  volume_type     = "gp3"
-  volume_size     = "50"
+#   mixed_instances = ["c6g.large"]
+#   volume_type     = "gp3"
+#   volume_size     = "50"
 
-  min = 1
-  max = 12
+#   min = 1
+#   max = 12
 
-  tags = local.tags
+#   tags = local.tags
 
-  depends_on = [
-    module.eks,
-  ]
-}
+#   depends_on = [
+#     module.eks,
+#   ]
+# }
