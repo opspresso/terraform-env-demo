8412571c68e86dc39bf6832db8d5b5c73ba825c8
nalbam <me@nalbam.com>
2024-12-30 15:28:59 +0900

    Update S3 bucket name for Terraform backend configurations to a new bucket across all demo environments

--- demo/7-eks/02-backend.tf
+++ demo/7-eks/02-backend.tf
@@ -5,7 +5,7 @@ terraform {
 
   backend "s3" {
     region         = "ap-northeast-2"
-    bucket         = "terraform-workshop-968005369378"
+    bucket         = "terraform-workshop-396608815058"
     key            = "backend/demo/eks-demo/terraform.tfstate" # for eks-demo
     dynamodb_table = "terraform-resource-lock"
     encrypt        = true
