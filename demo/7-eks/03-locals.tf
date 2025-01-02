ee17a492b5d995528b52939da38af9d09eabc181
nalbam <me@nalbam.com>
2025-01-02 20:05:25 +0900

    Refactor EKS node security group rules by commenting out unused ingress rules and adding new ALB ingress configurations for ports 80 and 15021. This update improves clarity and maintainability of the security group settings.

--- demo/7-eks/03-locals.tf
+++ demo/7-eks/03-locals.tf
@@ -14,27 +14,46 @@ locals {
   # ]
 
   node_security_group_additional_rules = {
-    ingress_all_self_node = {
-      description = "Node to node all"
-      protocol    = "-1"
-      from_port   = 0
-      to_port     = 0
-      type        = "ingress"
-      self        = true
-    }
-    ingress_all_self_cluster = {
-      description                   = "Cluster API to node all"
-      protocol                      = "-1"
-      from_port                     = 0
-      to_port                       = 0
+    # ingress_all_self_node = {
+    #   description = "Node to node all"
+    #   protocol    = "-1"
+    #   from_port   = 0
+    #   to_port     = 0
+    #   type        = "ingress"
+    #   self        = true
+    # }
+    # ingress_all_self_cluster = {
+    #   description                   = "Cluster API to node all"
+    #   protocol                      = "-1"
+    #   from_port                     = 0
+    #   to_port                       = 0
+    #   type                          = "ingress"
+    #   source_cluster_security_group = true
+    # }
+    # istio
+    ingress_cluster_15017_webhook = {
+      description                   = "Cluster API to node 15017/tcp webhook"
+      protocol                      = "tcp"
+      from_port                     = 15017
+      to_port                       = 15017
       type                          = "ingress"
       source_cluster_security_group = true
     }
-    ingress_all_self_alb = {
-      description              = "ALB to node all"
-      protocol                 = "-1"
-      from_port                = 0
-      to_port                  = 0
+    # alb http
+    ingress_alb_80 = {
+      description              = "ALB to node 80/tcp"
+      protocol                 = "tcp"
+      from_port                = 80
+      to_port                  = 80
+      type                     = "ingress"
+      source_security_group_id = data.terraform_remote_state.alb.outputs.security_group_id
+    }
+    # alb health
+    ingress_alb_15021 = {
+      description              = "ALB to node 15021/tcp"
+      protocol                 = "tcp"
+      from_port                = 15021
+      to_port                  = 15021
       type                     = "ingress"
       source_security_group_id = data.terraform_remote_state.alb.outputs.security_group_id
     }
