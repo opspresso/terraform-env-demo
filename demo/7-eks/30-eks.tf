717397f1fab80ad63eb57f5b92567a71d4395ccf
nalbam <me@nalbam.com>
2025-01-07 15:34:41 +0900

    Comment out unused cluster creator admin permissions in EKS configuration to enhance clarity and maintainability.

--- demo/7-eks/30-eks.tf
+++ demo/7-eks/30-eks.tf
@@ -16,7 +16,7 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
-  enable_cluster_creator_admin_permissions = true
+  # enable_cluster_creator_admin_permissions = true
 
   # # EKS Auto Mode
   # cluster_compute_config = {
