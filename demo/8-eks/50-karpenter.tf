ef658fe396a92f0b3b994c87cd966b2c5244eb53
nalbam <me@nalbam.com>
2026-07-22 00:32:39 +0900

    Add enable_inline_policy to Karpenter module configuration

--- demo/8-eks/50-karpenter.tf
+++ demo/8-eks/50-karpenter.tf
@@ -6,6 +6,8 @@ module "karpenter" {
 
   cluster_name = module.eks.cluster_name
 
+  enable_inline_policy = true
+
   # Attach additional IAM policies to the Karpenter node IAM role
   node_iam_role_additional_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
